6eba9b348237d48681e1a3f1544a00681efde088
minghan108 <liumh@mcmaster.com>
2018-11-01 16:22:15 -0400

    fix rope mechanic

--- Assets/RopeScript.cs
+++ Assets/RopeScript.cs
@@ -1,85 +1,127 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+
 
 public class RopeScript : MonoBehaviour {
-    public Vector2 destiny;
 
-    public float speed = 1;
+	public Vector2 destiny;
 
-    public float distance = 2;
-
-    public GameObject nodePrefab;
-
-    public GameObject player;
-
-    public GameObject lastNode;
-
-    bool done = false;
-
-    // Use this for initialization
-    void Start()
-    {
-
-        player = GameObject.FindGameObjectWithTag("Player");
-
-        lastNode = transform.gameObject;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+	public float speed= 1;
 
 
-        transform.position = Vector2.MoveTowards(transform.position, destiny, speed);
+	public float distance = 2;
+
+	public GameObject nodePrefab;
+
+	public GameObject player;
+
+	public GameObject lastNode;
 
 
+	public LineRenderer lr;
+
+	int vertexCount=2;
+	public List<GameObject> Nodes = new List<GameObject>();
 
 
-        if ((Vector2)transform.position != destiny)
-        {
+	bool done=false;
 
-            if (Vector2.Distance(player.transform.position, lastNode.transform.position) > distance)
-            {
+	// Use this for initialization
+	void Start () {
+	
 
+		lr = GetComponent<LineRenderer> ();
 
-                CreateNode();
+		player = GameObject.FindGameObjectWithTag ("Player");
 
-            }
-
-
-        }
-        else if (done == false)
-        {
-
-            done = true;
+		lastNode = transform.gameObject;
 
 
-            lastNode.GetComponent<HingeJoint2D>().connectedBody = player.GetComponent<Rigidbody2D>();
-        }
+		Nodes.Add (transform.gameObject);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		transform.position = Vector2.MoveTowards (transform.position,destiny,speed);
 
 
 
-    }
+
+		if ((Vector2)transform.position != destiny) {
+
+			if (Vector2.Distance (player.transform.position, lastNode.transform.position) > distance) {
 
 
-    void CreateNode()
-    {
+				CreateNode ();
 
-        Vector2 pos2Create = player.transform.position - lastNode.transform.position;
-        pos2Create.Normalize();
-        pos2Create *= distance;
-        pos2Create += (Vector2)lastNode.transform.position;
-
-        GameObject go = (GameObject)Instantiate(nodePrefab, pos2Create, Quaternion.identity);
+			}
 
 
-        go.transform.SetParent(transform);
+		} else if (done == false) {
 
-        lastNode.GetComponent<HingeJoint2D>().connectedBody = go.GetComponent<Rigidbody2D>();
-
-        lastNode = go;
+			done = true;
 
 
-    }
+
+			while(Vector2.Distance (player.transform.position, lastNode.transform.position) > distance)
+			{
+				CreateNode ();
+			}
+
+
+			lastNode.GetComponent<HingeJoint2D> ().connectedBody = player.GetComponent<Rigidbody2D> ();
+		}
+
+
+		RenderLine ();
+	}
+
+
+	void RenderLine()
+	{
+
+		lr.SetVertexCount (vertexCount);
+	
+		int i;
+		for (i = 0; i < Nodes.Count; i++) {
+
+			lr.SetPosition (i, Nodes [i].transform.position);
+
+		}
+
+		lr.SetPosition (i, player.transform.position);
+
+	}
+
+
+	void CreateNode()
+	{
+
+		Vector2 pos2Create = player.transform.position - lastNode.transform.position;
+		pos2Create.Normalize ();
+		pos2Create *= distance;
+		pos2Create += (Vector2)lastNode.transform.position;
+
+		GameObject go = (GameObject)	Instantiate (nodePrefab, pos2Create, Quaternion.identity);
+
+
+		go.transform.SetParent (transform);
+
+		lastNode.GetComponent<HingeJoint2D> ().connectedBody = go.GetComponent<Rigidbody2D> ();
+
+		lastNode = go;
+
+		Nodes.Add (lastNode);
+
+		vertexCount++;
+
+	}
+
+
+
 }
--- Assets/throwHook.cs
+++ Assets/throwHook.cs
@@ -1,37 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class throwHook : MonoBehaviour {
-
-    public GameObject hook;
-
-    GameObject curHook;
-
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+public class throwhook : MonoBehaviour {
 
 
-
-        if (Input.GetMouseButtonDown(0))
-        {
-
-            Vector2 destiny = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            curHook = (GameObject)Instantiate(hook, transform.position, Quaternion.identity);
-
-            curHook.GetComponent<RopeScript>().destiny = destiny;
+	public GameObject hook;
 
 
-        }
+	public bool ropeActive;
+
+	GameObject curHook;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
 
 
-    }
+		if (Input.GetMouseButtonDown (0)) {
+
+
+			if (ropeActive == false) {
+				Vector2 destiny = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+
+
+				curHook = (GameObject)Instantiate (hook, transform.position, Quaternion.identity);
+
+				curHook.GetComponent<RopeScript> ().destiny = destiny;
+
+
+				ropeActive = true;
+			} else {
+
+				//delete rope
+
+				Destroy (curHook);
+
+
+				ropeActive = false;
+
+			}
+		}
+
+
+	}
 }
