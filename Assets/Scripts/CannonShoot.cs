b942123e7e31afb889a0dbca2e7f5d5428f4dfec
Dustin Jurkaulionis <dustinjurkaulionis@gmail.com>
2018-11-28 00:26:54 -0500

    buggy cannon

---
+++ Assets/Scripts/CannonShoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonShoot : MonoBehaviour {
+    public GameObject cannonBall;
+    public float firePower;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShootCannon()
+    {
+        GameObject thisCannonBall = Instantiate(cannonBall, transform.position, transform.rotation);
+        thisCannonBall.GetComponent<Rigidbody2D>().AddRelativeForce(Vector3.back*firePower);
+    }
+}
