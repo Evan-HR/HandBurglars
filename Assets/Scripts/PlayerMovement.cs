4d22fe1d68a0ce6339d29b9fbfa885d30b16adea
Dustin Jurkaulionis <dustinjurkaulionis@gmail.com>
2019-01-02 17:46:37 -0500

    Dustin and Evan's #MegaPush of 2019 aka fixed left ladder functioanlity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour
+{
 
     public CharacterController2D controller;
     public float runSpeed = 40f;
@@ -18,13 +19,16 @@ public class PlayerMovement : MonoBehaviour {
     //ladder vaiables 
     public float distance;
     public LayerMask whatIsLadder;
+    public LayerMask whatIsLadderTop;
     private bool isClimbing;
 
-    void Start () {
+    void Start()
+    {
         rb = gameObject.GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
         Debug.Log("Player1 move :" + horizontalMove);
@@ -37,11 +41,12 @@ public class PlayerMovement : MonoBehaviour {
         if (Input.GetButtonDown("Crouch"))
         {
             crouch = true;
-        } else if (Input.GetButtonUp("Crouch"))
+        }
+        else if (Input.GetButtonUp("Crouch"))
         {
             crouch = false;
         }
-        
+
     }
 
     void FixedUpdate()
@@ -50,29 +55,43 @@ public class PlayerMovement : MonoBehaviour {
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
 
-        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position,Vector2.up,distance,whatIsLadder);
-        if(hitInfo.collider != null)
+        // setting isClimbing to true or false
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, whatIsLadder);
+       // RaycastHit2D topLadder = Physics2D.Raycast(transform.position, Vector2.up, distance, whatIsLadderTop);
+        if (hitInfo.collider != null) // means we're on/in the ladder hitbox
         {
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
             {
                 isClimbing = true;
-              
+
             }
-            else if ((Input.GetButtonDown("Jump")) || (hitInfo.collider == null))
+            else if ((Input.GetButtonDown("Jump")))
             {
                 isClimbing = false;
-                rb.gravityScale = 3;
             }
+        }
+        else
+        {
+            isClimbing = false;
+        }
 
-            if(isClimbing)
-            {
-                verticalMove = Input.GetAxisRaw("Vertical");
-                rb.velocity = new Vector2(rb.velocity.x, verticalMove * climbSpeed);
+        if (isClimbing)
+        {
+            Debug.Log("climbing is: " + isClimbing);
+            Debug.Log("gravity is:" + rb.gravityScale);
+            verticalMove = Input.GetAxisRaw("Vertical");
+            rb.velocity = new Vector2(rb.velocity.x, verticalMove * climbSpeed);
 
-                
-                rb.gravityScale = 0;   //so the player doesn't fall down when on ladder
-                rb.velocity.Set(rb.velocity.x, 0); // gets rid of residual effects from gravity 
-            }
+
+            rb.gravityScale = 0;   //so the player doesn't fall down when on ladder
+            rb.velocity.Set(rb.velocity.x, 0); // gets rid of residual effects from gravity 
+        }
+        else
+        {
+            Debug.Log("climbing is: " + isClimbing);
+            rb.gravityScale = 3;
+            Debug.Log("gravity is:" + rb.gravityScale);
         }
     }
 }
