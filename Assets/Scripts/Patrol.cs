b5170c6f10f71f59bd6665475a41066c55fd1d6c
minghan108 <liumh@mcmaster.com>
2019-01-08 15:35:15 -0500

    integrate with PlayerController script to getHideStatus

--- Assets/Scripts/Patrol.cs
+++ Assets/Scripts/Patrol.cs
@@ -12,6 +12,7 @@ public class Patrol : MonoBehaviour {
     private int randomSpot;
     private Vector3 playerPosition;
     public int playerHandLayer;
+    private PlayerController playerController;
     public enum DetectionState{
         NOT_DETECTED,
         DETECTED
@@ -21,7 +22,7 @@ public class Patrol : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         playerHandLayer = ~(LayerMask.GetMask("Hand"));
- 
+        playerController = GameObject.FindObjectOfType<PlayerController>();
         waitTime = startWaitTime;
         randomSpot = 0;
         //randomSpot = Random.Range(0, moveSpots.Length);
@@ -29,51 +30,61 @@ public class Patrol : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        RaycastHit2D detectionRaycast = Physics2D.Raycast(transform.position, transform.right, distance, playerHandLayer);
-        Debug.Log("HandState = DISABLED");
-        //controls direction of raycast based on which MoveSpot Critter is moving towards
-        if ( randomSpot == 1)
+        if (!playerController.getHideStatus())
         {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-        }
-        else if (randomSpot == 0)
-        {
-            transform.rotation = Quaternion.Euler(0, 180f, 0);
-        }
+            RaycastHit2D detectionRaycast = Physics2D.Raycast(transform.position, transform.right, distance, playerHandLayer);
 
-
-        if (detectionRaycast.collider != null)
-        {
-            Debug.DrawLine(transform.position, detectionRaycast.point, Color.red);
-            playerPosition = detectionRaycast.collider.gameObject.transform.position;
-            transform.position = Vector2.MoveTowards(transform.position, playerPosition, enemyDetectionSpeed * Time.deltaTime);
-        }
-        else
-        {
-            Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.green);
-
-            transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
-
-            if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+            Debug.Log("HandState = DISABLED");
+            //controls direction of raycast based on which MoveSpot Critter is moving towards
+            if (randomSpot == 1)
             {
-                if (waitTime <= 0)
-                {
-                    if (randomSpot == 0)
-                    {
-                        randomSpot = 1;
-                    } else
-                    {
-                        randomSpot = 0;
-                    }
-                    //randomSpot = Random.Range(0, moveSpots.Length);
-                    waitTime = startWaitTime;
-                }
-                else
-                {
-                    waitTime -= Time.deltaTime;
-                }
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else if (randomSpot == 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 180f, 0);
+            }
+
+
+            if (detectionRaycast.collider != null)
+            {
+                Debug.DrawLine(transform.position, detectionRaycast.point, Color.red);
+                playerPosition = detectionRaycast.collider.gameObject.transform.position;
+                transform.position = Vector2.MoveTowards(transform.position, playerPosition, enemyDetectionSpeed * Time.deltaTime);
+            }
+            else
+            {
+                Debug.DrawLine(transform.position, transform.position + transform.right * distance, Color.green);
+
             }
         }
 
-	}
+        MoveToMoveSpot();    
+    }
+
+    private void MoveToMoveSpot()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                if (randomSpot == 0)
+                {
+                    randomSpot = 1;
+                }
+                else
+                {
+                    randomSpot = 0;
+                }
+                //randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
 }
