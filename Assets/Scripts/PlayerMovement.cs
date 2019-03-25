54f7e780a0122ef31eeab3051ec823fe6fd9be1d
Dustin Jurkaulionis <dustinjurkaulionis@gmail.com>
2019-03-24 20:04:07 -0400

    awesome cam script added to level 1, will tweak on mar 25th

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementtemp : MonoBehaviour
 {
 
     public CharacterController2D controller;
